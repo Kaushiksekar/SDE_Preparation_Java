855413329c0441a090909c97b95340265355422d
kaushik3993 <kaushik3993@gmail.com>
2020-08-25 00:16:29 +0530

    Refactored services to have a common parent interface. Closes #11

--- spring-boot/spring-pet-clinic/pet-clinic-data/src/main/java/com/tutorials/springboot/services/VetService.java
+++ spring-boot/spring-pet-clinic/pet-clinic-data/src/main/java/com/tutorials/springboot/services/VetService.java
@@ -2,13 +2,6 @@ package com.tutorials.springboot.services;
 
 import com.tutorials.springboot.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
